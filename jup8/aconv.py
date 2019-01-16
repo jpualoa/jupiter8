88fc3be5189685942a913a6c9f1d1c3b635c37ed
Jason <jpualoa@vt.edu>
2019-01-15 19:22:30 -0500

    initial commit

---
+++ jup8/aconv.py
@@ -0,0 +1,32 @@
+# ==============================================================================
+# Build an A-ConvNets network
+#
+# Reference: "Target Classification Using the Deep Convolutional Networks for
+#             SAR Images" by Chen et al.
+# ==============================================================================
+# Sandard imports
+
+# External dependencies
+from keras.models import Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout
+from keras import Input
+
+INPUT_SHAPE = (88,88,1)
+
+def get_aconv(input_shape=INPUT_SHAPE):
+    input_tensor = Input(shape=input_shape)
+    x = Conv2D(16, (5,5), activation='relu')(input_tensor)
+    x = MaxPooling2D((2,2))(x)
+    x = Conv2D(32, (5,5), activation='relu')(x)
+    x = MaxPooling2D((2,2))(x)
+    x = Conv2D(64, (6,6), activation='relu')(x)
+    x = MaxPooling2D((2,2))(x)
+    x = Dropout(0.5)(x)
+    x = Conv2D(128, (5,5), activation='relu')(x)
+    x = Conv2D(10, (3,3), activation='softmax')(x)
+    model = Model(input_tensor, x)
+    return model
+
+if __name__ == '__main__':
+    model = get_aconv()
+    model.summary()
