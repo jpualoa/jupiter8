f3c7b8e623bc06d9f9d13a1c458443ea7a89e5bf
jason <jason@br-envision.com>
2019-01-26 12:04:20 -0500

    fixed to handle image shape correctly

--- jup8/imgproc/filters.py
+++ jup8/imgproc/filters.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-def center_crop(input_img, out_shape):
+def center_crop(input_img, out_width, out_height):
     """Return a center cropped copy of an image array
 
     Args:
-        input_img:    numpy array
-        output_shape: outp dimensions (x,y)
+        input_img:  numpy array
+        out_width:  output x dimension
+        out_height: output y dimension
     """
     img = np.copy(input_img)
     x, y = img.shape[:2]
-    new_x, new_y = out_shape
-    if (new_x > x) or (new_y > y):
+    if (out_width > x) or (out_height > y):
         raise RuntimeError("Output shape must be smaller than img shape")
-    dx = x - new_x
-    dy = y - new_y
-    cropped = img[dx/2: x - dx/2, dy/2: y - dy/2]
+    dx = x - out_width
+    dy = y - out_height
+    cropped = img[dx/2: x - dx/2, dy/2: y - dy/2, :]
     return cropped
 
