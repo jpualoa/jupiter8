6ccafd90a95ee4dd61ea71e89b7b740f5439249a
jason <jason@br-envision.com>
2019-04-14 13:55:26 -0400

    updates...yadda

--- jup8/utils/argutils.py
+++ jup8/utils/argutils.py
@@ -5,3 +5,15 @@ def valid_path(pathname):
     if not os.path.exists(pathname):
         raise ValueError("%s does not exist" % pathname)
     return pathname
+
+
+def valid_list(val, n=None):
+    """Returns 'val', a comma-delimited string, as a list of strings or raises a
+    ValueError"""
+    try:
+        if type(val) is not list: val = [str(x) for x in val.split(',')]
+    except (ValueError, TypeError): raise ValueError("Invalid value: %s" %val)
+
+    if n and len(val) != n:
+        raise ValueError("Argument requires %s elements" %n)
+    return val
